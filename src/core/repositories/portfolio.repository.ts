7c010650f22d28fa60054fbc4868e19df59bbf09
aardaipek <ardaipek66@gmail.com>
2023-05-29 12:57:40 +0300

    createActivity EP added

--- src/core/repositories/portfolio.repository.ts
+++ src/core/repositories/portfolio.repository.ts
@@ -3,13 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IPortfolio } from '../interfaces/portfolio.interface';
 import { CreatePortfolioDto } from '../dto/create.portfolio';
- 
+
 @Injectable()
 export class PortfolioRepository {
-    constructor(@InjectModel('portfolio') private portfolioModel: Model<IPortfolio>) {}
+  constructor(@InjectModel('portfolio') private portfolioModel: Model<IPortfolio>,) {}
 
-
-  public async createPortfolio(createPortfolioDto: CreatePortfolioDto): Promise<IPortfolio> {
+  public async createPortfolio(
+    createPortfolioDto: CreatePortfolioDto,
+  ): Promise<IPortfolio> {
     const newPortfolio = await new this.portfolioModel(createPortfolioDto);
     return newPortfolio.save();
   }
